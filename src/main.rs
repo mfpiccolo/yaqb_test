b23788da92380f9df6bf68f94e7423ab636535e7
Mike Piccolo <mfpiccolo@gmail.com>
2015-11-26 21:13:38 -0800

    Initial setup for yaqb backed server

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,100 @@
-extern crate dotenv;
-#[macro_use] extern crate yaqb;
+#![feature(custom_derive, plugin)]
+#![plugin(serde_macros)]
+#![plugin(yaqb_codegen)]
 
+#[macro_use] extern crate yaqb;
+#[macro_use] extern crate nickel;
+extern crate dotenv;
+extern crate serde;
+extern crate serde_json;
+extern crate rustc_serialize;
+use rustc_serialize::json;
+
+use nickel::{Nickel,
+  HttpRouter,
+  Request,
+  Response,
+  MiddlewareResult,
+  Continue,
+  JsonBody
+};
+
+mod models;
+use models::user::{User, NewUser};
 use yaqb::*;
 
-#[derive(PartialEq, Eq, Debug, Clone)]
-pub struct User {
-    pub id: i32,
-    pub name: String,
-    pub hair_color: Option<String>,
-}
-
-table! {
-    users {
-        id -> Serial,
-        name -> VarChar,
-        hair_color -> Nullable<VarChar>,
-    }
-}
-
-queriable! {
-    User {
-        id -> i32,
-        name -> String,
-        hair_color -> Option<String>,
-    }
-}
-
 fn main() {
-  use self::users::table as users;
   dotenv::dotenv().ok();
-  let connection_url = ::std::env::var("DATABASE_URL").ok()
-      .expect("DATABASE_URL must be set in order to run tests");
-  let conn = Connection::establish(&connection_url).unwrap();
+  let mut server = Nickel::new();
 
-  conn.execute("INSERT INTO users (name) VALUES ('Sean'), ('Tess')")
-      .unwrap();
+  server.utilize(middleware! { |request|
+      println!("logging request from middleware! macro: {:?}", request.origin.uri);
+  });
 
-  let result: Option<User> = conn.find(users, 1).unwrap();
+  server.utilize(logger);
 
-  println!("{:?}", result);
+  let mut router = Nickel::router();
+
+  router.get("/users/:userid", middleware! { |request|
+    let user_id = request.param("userid").unwrap().parse::<i32>().unwrap();
+    User::find(user_id).unwrap().to_json()
+  });
+  router.get("/users", middleware!(User::count().unwrap().to_string()));
+
+  // try it with curl
+  // curl 'http://localhost:6767/users' -H 'Content-Type: application/json;charset=UTF-8'  --data-binary $'{ "name": "John","email": "Connor" }'
+  router.post("/users", middleware! { |request, response|
+    let new_user = request.json_as::<NewUser>().unwrap();
+    let new_users = vec!(new_user);
+    User::insert(new_users);
+  });
+
+  server.utilize(router);
+  server.listen("127.0.0.1:6767");
 }
+
+fn logger<'mw>(req: &mut Request, res: Response<'mw>) -> MiddlewareResult<'mw> {
+  println!("logging request from logger fn: {:?}", req.origin.uri);
+  Ok(Continue(res))
+}
+
+#[test]
+fn find_user_test() {
+  let conn = before_each();
+  let mike: User = User::find(1).unwrap();
+  assert_eq!("Mike", mike.name);
+  after_each(&conn);
+}
+
+#[test]
+fn count_users_test() {
+  let conn = before_each();
+  assert_eq!(Some(2), User::count());
+  after_each(&conn);
+}
+
+fn setup_users_table(conn: &Connection) {
+  conn.execute("CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    email VARCHAR
+  )").unwrap();
+}
+
+fn tear_down_users_table(conn: &Connection) {
+  conn.execute("DROP TABLE IF EXISTS users").unwrap();
+}
+
+fn before_each() -> Connection {
+  dotenv::dotenv().ok();
+  let conn = User::conn();
+  setup_users_table(&conn);
+  conn.execute("INSERT INTO users (name) VALUES ('Mike'), ('Joe')").unwrap();
+  User::conn()
+}
+
+fn after_each(conn: &Connection) {
+  // tear_down_users_table(&conn);
+}
+
 
---
+++ src/models/user.rs
@@ -0,0 +1,79 @@
+extern crate serde;
+extern crate serde_json;
+
+use yaqb::*;
+
+table! {
+  users {
+    id -> Serial,
+    name -> VarChar,
+    email -> Nullable<VarChar>,
+  }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone, Queriable)]
+pub struct User {
+  pub id: i32,
+  pub name: String,
+  pub email: Option<String>,
+}
+
+impl User {
+
+  #[inline]
+  pub fn conn() -> Connection {
+    let connection_url = ::std::env::var("DATABASE_URL").ok()
+      .expect("DATABASE_URL must be set in order to run tests");
+    Connection::establish(&connection_url).unwrap()
+  }
+
+  pub fn find(id: i32) -> Option<User> {
+    User::conn().find(self::users::table, id).unwrap()
+  }
+
+  pub fn count() -> Option<i64> {
+    let select_count = users::table.select_sql::<types::BigInt>("COUNT(*)");
+    User::conn().query_one::<_, i64>(select_count.clone()).unwrap()
+  }
+
+  pub fn insert(new_users: Vec<NewUser>) {
+
+    User::conn().insert(&self::users::table, new_users).unwrap().collect();
+  }
+
+  pub fn to_json(&self) -> String {
+    serde_json::to_string(&self).unwrap()
+  }
+
+}
+
+#[derive(RustcDecodable, Serialize, Deserialize, PartialEq, Eq, Debug, Clone, Queriable)]
+pub struct NewUser {
+  pub name: String,
+  pub email: Option<String>,
+}
+
+impl NewUser {
+  pub fn new(name: &str, email: Option<&str>) -> Self {
+    NewUser {
+      name: name.to_string(),
+      email: email.map(|s| s.to_string()),
+    }
+  }
+}
+
+insertable! {
+  NewUser => users {
+    name -> String,
+    email -> Option<String>,
+  }
+}
+
+changeset! {
+  NewUser => users {
+    name -> String,
+    email -> Option<String>,
+  }
+}
+
+
